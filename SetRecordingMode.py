8bf9891876bb14324e737158fbeaccac71cdb5c1
Carlosm <carlmukoyi@gmail.com>
2023-11-17 10:12:47 +0200

    remove secrets

--- SetRecordingMode.py
+++ SetRecordingMode.py
@@ -10,7 +10,7 @@ url = "https://poc.connectedcar360.net/v1/api/Policies/SetRecordingMode"
 
 headers = {
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer xNQrlUsvcgCdxAv_EJbCGbwkWsPHAtYqfKMK1ipHuTmrZI3mwKzIBThMwGn5fqLDOmQe68IGU_kF3rd3Qq_unnIfO1dyht6A79QdJhNMml_YC1zisttaTtGAXZpBE17RVoFazLpAtOOD5GKuduEMarOUxEuWWQZa2GswOzc1LemnfxMeqZskROvvHkrOYNdqxNdpzOipGIQIzXTo_Mj765998SL27zdjp_80kOcPzTLiYkt7qWdk4vjgptOU7RPLIFbQ6htvsCl3c-RDtch9JrSoxQwluvslnzEA9l7SFcFebY7jnZ_8cqGtesxLgjRBxMLgkhc6F71mnwxjhTvKXhbpKu2hsOll3iiAo9YdlGTaYrucOlxw28NlFZg2Z1GJmyFVtPQi42FiXkOedT5TZQN6b9mDflJCHh6YsFuX2HQGSHmaZR1HoVHq_E_QjIGKLlSBZXvkQAvbyAMrAEBm6-OHlOm5eAARgd2N7-SwxpvVb5RbhQIrFsr-PBrSJQm6xLqTxrLVGnHlKwd8chsm0A'
+    'Authorization': ''
 }
 
 # Get the current date for creating the log file
